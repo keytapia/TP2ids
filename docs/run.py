de0041af7bc2cb95ecb8cb411719fc69179944dd
keytapia <ktapia@fi.uba.ar>
2026-04-11 15:32:38 -0300

    agregue algo a config.py

--- docs/run.py
+++ docs/run.py
@@ -1,7 +1,8 @@
-#para arrancar el servidor
+#para arrancar el servidor y visualizar en postman o navegador
 from app.app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
