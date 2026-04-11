113787774578a4e69e0ec3e0c8d4bc9f8bea5a8b
keytapia <ktapia@fi.uba.ar>
2026-04-11 15:36:49 -0300

    hagan pip install pymysql

--- docs/config.py
+++ docs/config.py
@@ -1,5 +1,5 @@
 # configuracion general, conexion MySQL y settings
 class Config:
     # Configuración de la base de datos MySQL
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/myapp'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/myapp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
