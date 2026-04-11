1211402f90a4311eafd0c1912b4a454e5cff3cec
keytapia <ktapia@fi.uba.ar>
2026-04-11 18:47:24 -0300

    carpetas

--- docs/app/routes/main.py
+++
@@ -1,8 +0,0 @@
-#esto es solo de prueba para ver con python run.py si todo funciona y si Flask corre
-from flask import Blueprint
-
-main_bp = Blueprint("main", __name__)
-
-@main_bp.route("/", methods=["GET"])
-def inicio():
-    return "Hola mondo"
