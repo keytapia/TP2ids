0a5cbc8ba38ed5062c50360ef22735f094b95b84
keytapia <ktapia@fi.uba.ar>
2026-04-11 12:41:53 -0300

    Add 'fase' and goal columns to partidos table
    Definicion de las tablas para guardar los datos y uso de CONSTRAINT para que no se repita el mismo usuario con el mismo partido

--- docs/database.sql
+++ docs/database.sql
@@ -12,3 +12,27 @@ CREATE TABLE partidos (
   equipo_local VARCHAR(100) NOT NULL,
   equipo_visitante VARCHAR(100) NOT NULL,
   fecha DATETIME NOT NULL,
+  fase VARCHAR(50) NOT NULL,
+  goles_local INT,
+  goles_visitantes INT
+);
+
+CREATE TABLE predicciones (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  usuario_id INT NOT NULL,
+  partido_id INT NOT NULL,
+  pred_local INT NOT NULL,
+  ped_visitante INT NOT NULL
+  
+  CONSTRAINT unique_prediccion UNIQUE(usuario_id, partido_id),
+  
+  CONSTRAINT fk_usuario
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+  
+  CONSTRAINT fk_partido
+    FOREIGN KEY (partido_id)
+    REFERENCES partidos(id)
+    ON DELETE CASCADE
+  );
