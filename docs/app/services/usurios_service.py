b15099d6874db4c0e72e63550993ed342229c4fd
mnov25 <mnovillo@fi.uba.ar>
2026-04-18 21:36:08 +0200

    DELETE usurios_service.py

--- docs/app/services/usurios_service.py
+++ docs/app/services/usurios_service.py
@@ -1,1 +1,15 @@
 
+from app.db import db
+from app.models.usuario import usuario
+
+#DELETE
+
+def eliminar_usuario(id):
+    usuario_encontrado=usuario.query.get(id)
+    if not usuario_encontrado:
+        return 0
+    
+    db.session.delete(usuario_encontrado)
+    db.session.commit()
+
+    return 1
