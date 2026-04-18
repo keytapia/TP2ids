290e3f988aac16ab66a3a74b9589fe2669fec38e
Miguel Martin <mmartin2003.98@gmail.com>
2026-04-18 10:12:44 +0000

    se creo la clase usuario en models, las validaciones de usuario en service y el POST de usuario en routes

--- docs/app/routes/usuarios.py
+++ docs/app/routes/usuarios.py
@@ -1,1 +1,32 @@
+from flask import Flask, request, jsonify, Blueprint
+from app.services.usurios_service import crear_usuario
+from app.db import db
+
+usuarios_bp = Blueprint("usuarios", __name__)
+
+
+@usuarios_bp.route("/usuarios", methods=["POST"])
+def crear_usuario():
+    dato = request.get_json()
+    nuevo_usuario = crear_usuario(dato)
+    if "error" in nuevo_usuario:
+        error = {
+            "errors": [
+                {
+                    "code": nuevo_usuario.get("code.api"),
+                    "message": crear_usuario.get("error"),
+                    "level": "error",
+                    "descripcion": crear_usuario.get("description")
+                }
+            ]
+        }
+        return jsonify(error)
+    return jsonify(nuevo_usuario, 201)
+
+
+
+
+
+
+
 
--- docs/app/services/usurios_service.py
+++ docs/app/services/usurios_service.py
@@ -1,1 +1,41 @@
+from app.db import db
+from app.models.usuario import usuario
+#import re (no se si hay que ponerlo para que funcione)
+import re
 
+
+def crear_usuario(dato):
+
+    if not dato.get('nombre'):
+        return {
+            'code': 400,
+            'message': 'Faltan datos obligatorios',
+            'level': 'error',
+            'description': 'El campo "nombre" no fue completado y es obligatorio'
+        }
+    if not dato.get('email'):
+        return {
+            'code': 400,
+            'message': 'Faltan datos obligatorios',
+            'level': 'error',
+            'description': 'El campo "email" no fue completado y es obligatorio'
+        }
+    if usuario.query.filter_by(email=dato.get('email')).first():
+        return {
+            'code': 409,
+            'message': 'Este email ya esta siendo utilizado',
+            'level': 'error',
+            'description': 'Este email ya esta siendo utilizado'
+        }
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", dato.get('email')):
+        return {
+            'code': 400,
+            'message': 'Email invalido',
+            'level': 'error',
+            'description': 'el formato de email es invalido'
+        }
+
+    nombre = dato.get('nombre')
+    email = dato.get('email')
+    nuevo_usuario = db.crear(nombre, email)
+    return nuevo_usuario, 201
