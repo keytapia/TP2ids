9842f43ca781c9fc5361b9425c970dfcf483633b
keytapia <ktapia@fi.uba.ar>
2026-04-19 11:47:00 -0300

    delete models

--- docs/app/models/usuario.py
+++
@@ -1,7 +0,0 @@
-from app.db import db
-
-class usuario(db.Model):
-    __tablename__ = "usuarios"
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(200), nullable=False, unique=True)
