2593e592e3d7b8a725f2776eccc2657677d13614
keytapia <ktapia@fi.uba.ar>
2026-04-19 19:13:51 -0300

    tube que hacer unos pasos con mysql para crear una contraseña e instalar un requerimiento nuevo para que todo funcione

--- docs/config.py
+++ docs/config.py
@@ -1,5 +1,5 @@
 # Donde vamos a guardar los datos de conexion a la base
 DB_HOST = "localhost"
-DB_USER = "tu_usuario"
-DB_PASSWORD = "tu_contraseña"
+DB_USER = "root"
+DB_PASSWORD = "patata2026"
 DB_NAME = "prode_db"
